d5cd99e1399f1704f974d28e73d9855f23350276
kanksha44 <kanksha44@gmail.com>
2024-01-12 00:53:55 +0530

    initilaized

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Routing from "./Routing/Routing";
+function App() {
+  return (
+    <>
+      <div>
+        <Routing />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import HomePage from "../HomePage/HomePage";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+    </div>
+  );
+};
+
+export default Home;
