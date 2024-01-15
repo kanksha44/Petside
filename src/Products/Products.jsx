c8bb36edca1b72d2fd32f598823357b6d716dde9
kanksha44 <kanksha44@gmail.com>
2024-01-16 01:50:17 +0530

    "features:"product"

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,7 +1,9 @@
+import line from "../assets/Line15.png";
 const data = [
   {
     discount: "10%",
     img: "/src/assets/image12.png",
+    bgimg: "/src/assets/Vector.png",
     headline: "V-dog | Drools",
     discountedPrice: "Rs. 999",
     price: "MRP Rs. 1500",
@@ -9,6 +11,7 @@ const data = [
   {
     discount: "10%",
     img: "/src/assets/image12.png",
+    bgimg: "/src/assets/Vector.png",
     headline: "V-dog | Drools",
     discountedPrice: "Rs. 999",
     price: "MRP Rs. 1500",
@@ -16,6 +19,7 @@ const data = [
   {
     discount: "10%",
     img: "/src/assets/image12.png",
+    bgimg: "/src/assets/Vector.png",
     headline: "V-dog | Drools",
     discountedPrice: "Rs. 999",
     price: "MRP Rs. 1500",
@@ -23,6 +27,7 @@ const data = [
   {
     discount: "10%",
     img: "/src/assets/image12.png",
+    bgimg: "/src/assets/Vector.png",
     headline: "V-dog | Drools",
     discountedPrice: "Rs. 999",
     price: "MRP Rs. 1500",
@@ -31,38 +36,49 @@ const data = [
 
 const Products = () => {
   return (
-    <div>
-      <div className="container">
-        <div className="row d-flex flex-column">
-          <div className="col text-center border mb-5">
-            <h4>Pet </h4>
-            <h5>Food Products</h5>
-            <h5>Upto 25% OFF on all Products</h5>
-            <button>View More</button>
-          </div>
-          <div className="col border d-flex justify-content-between">
-            {data.map((item, index) => (
-              <div key={index} className="card border-success mb-3">
-                <div className="card-header d-flex justify-content-between order-success">
-                  <p>{item.discount}</p>
-                  <p>♥️</p>
-                </div>
-                <div className="card-body text-success">
-                  <img src={item.img} alt="" />
-                  <h5 className="card-title">{item.headline}</h5>
-                  <p className="card-text">{item.discountedPrice}</p>
-                  <p className="card-text">{item.price}</p>
-                </div>
-                <div className="card-footer border-success">
-                  <button>Add to cart</button>
-                  <button>Buy Now</button>
-                </div>
-              </div>
-            ))}
+    <section id="product" className="product">
+      <div className="container py-5">
+        <div className="row product-content">
+          <h4>Pet </h4>
+          <h4>Food Products</h4>
+          <img src={line} alt={line} className="service-line" />
+          <div className="d-flex justify-content-between align-items-center product-text">
+            <p>Upto 25% OFF on all Products</p>
+            <button className="product-btn">View More</button>
           </div>
         </div>
+        <div className="row mx-auto">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="product-card col-sm-6 col-lg-2 col-md-3"
+            >
+              <div className="card-header d-flex justify-content-between order-success">
+                <p>{item.discount}</p>
+                <p>
+                  <i className="bi bi-heart"></i>
+                </p>
+              </div>
+              <div className="card-body text-success">
+                <img src={item.img} alt="" className="product-img" />
+                <img src={item.bgimg} alt={item.bgimg} className="bgimg" />
+                <h5 className="card-title text-center text-black mt-5">
+                  {item.headline}
+                </h5>
+                <p className="card-text text-center text-black">
+                  {item.discountedPrice}
+                </p>
+                <p className="card-text text-center text-black">{item.price}</p>
+              </div>
+              <div className="card-footer">
+                <button>Add to cart</button>
+                <button>Buy Now</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -2,7 +2,7 @@ import Home from "../Home/Home";
 import Advice from "../Advice/Advice";
 import Banner from "../Banner/Banner";
 // import Customers from "../Customers/Customers";
-// import Products from "../Products/Products";
+import Products from "../Products/Products";
 // import Promo from "../Promo/Promo";
 // import Read from "../Read/Read";
 import Slider from "../Slider/Slider";
@@ -16,8 +16,8 @@ function Routing() {
         <Slider />
         <Banner />
         <Advice />
-        {/*<Products />
-        <Promo />
+        <Products />
+        {/* <Promo />
         <Read />
         <Customers />
         <Footer /> */}
